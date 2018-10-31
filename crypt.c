2fc7c4bda479ac3f76d931a73d4c36b2410b86c1
aojsd <michael.wu99@gmail.com>
2018-10-31 18:03:00 -0400

    Add files via upload

--- crypt.c
+++ crypt.c
@@ -12,6 +12,8 @@
 
 dev_t main_dev;
 struct cdev cryptctl;
+struct class *CryptClass;
+struct device *cryptmain;
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michael Wu");
@@ -19,12 +21,12 @@ MODULE_DESCRIPTION("OS Assignment 2: Encrypt/Decrypt Pseudo-Device Driver");
 
 // define file operations
 struct file_operations crypt_fops = {
-    .owner = THIS_MODULE;
-    .read = crypt_read;
-    .write = crypt_write;
-    .ioctl = crypt_ioctl;
-    .open = crypt_open;
-    .release = crypt_release;
+    //.owner = THIS_MODULE,
+    //.read = crypt_read,
+    //.write = crypt_write,
+    //.ioctl = crypt_ioctl,
+    //.open = crypt_open,
+    //.release = crypt_release,
 };
 
 static int __init cryptctl_init(void){
@@ -32,25 +34,53 @@ static int __init cryptctl_init(void){
     int result = alloc_chrdev_region(&main_dev, 0, 1, "cryptctl");
     crypt_major = MAJOR(main_dev);
     if(result < 0){
-        return result;
+        printk(KERN_NOTICE "Cryptctl: Error allocating major number\n");
+        goto ERR_MAJORNUM;
+    }
+
+    // create device class
+    CryptClass = class_create(THIS_MODULE, "CS416 P2");
+    if(IS_ERR(CryptClass)){
+        printk(KERN_NOTICE "Cryptctl: Error creating class\n");
+        goto ERR_CLASS;
+    }
+
+    // create device
+    cryptmain = device_create(CryptClass, NULL, main_dev, NULL, "cryptctl");
+    if(IS_ERR(cryptmain)){
+        printk(KERN_NOTICE "Cryptctl: Error creating device\n");
+        goto ERR_DEVICE_1;
     }
 
     // add cryptctl as a device driver
     cdev_init(&cryptctl, &crypt_fops);
     cryptctl->owner = THIS_MODULE;
     int err = cdev_add(&cryptctl, main_dev, 1);
-    if(err){
-        printk(KERN_DEBUG "Error adding cryptctl\n");
-        cdev_del(&cryptctl);
-        unregister_chrdev_region(main_dev, 1);
-        return -1;
+    if(err < 0){
+        printk(KERN_DEBUG "Cryptctl: Error adding cryptctl\n");
+        goto ERR_DEVICE_2;
     }
-    printk(KERN_DEBUG "Cryptctl initialized correctly.\n");
+
+    printk(KERN_DEBUG "Cryptctl: Initialized correctly.\n");
     return 0;
+
+    ERR_DEVICE_2:
+        cdev_del(&cryptctl);
+        device_destroy(cryptmain);
+    ERR_DEVICE_1:
+        class_unregister(CryptClass);
+        class_destroy(CryptClass);
+    ERR_CLASS:
+        unregister_chrdev_region(main_dev, 1);
+    ERR_MAJORNUM:
+        return -1;
 }
 
 static int __exit cryptctl_exit(void){
     cdev_del(&cryptctl);
+    device_destroy(cryptmain);
+    class_unregister(CryptClass);
+    class_destroy(CryptClass);
     unregister_chrdev_region(main_dev, 1);
     printk(KERN_DEBUG "Exit Success\n");
     return 0;
