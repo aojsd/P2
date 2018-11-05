a561c7a970ec93895497d4ed8337e0bbbda800cf
Michael Wu <michael.wu99@gmail.com>
2018-11-04 21:32:08 -0500

    Fixed editing string literals problem

--- .cryptctl.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/michael/Documents/P2/cryptctl.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/michael/Documents/P2/cryptctl.ko /home/michael/Documents/P2/cryptctl.o /home/michael/Documents/P2/cryptctl.mod.o ;  true
--- .tmp_versions/cryptctl.mod
+++
@@ -1,3 +0,0 @@
-/home/michael/Documents/P2/cryptctl.ko
-/home/michael/Documents/P2/cryptctl.o
-
